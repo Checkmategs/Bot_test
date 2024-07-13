fcdaf4cdad6d4e8fdb42df9f9e3c6dd52488c7e5
George Savchak <georgecloud@Users-MacBook-Air-3.local>
2024-07-13 19:15:15 +0300

    ADDED changes 13.08.2024

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ window.Telegram.WebApp.ready();
 document.getElementById('getUserInfo').addEventListener('click', function() {
     const user = Telegram.WebApp.initDataUnsafe.user;
     if (user) {
+        const avatar = document.getElementById('avatar');
+        if (user.photo_url) {
+            avatar.src = user.photo_url;
+            avatar.classList.remove('hidden');
+        }
         document.getElementById('firstName').textContent = user.first_name || 'Не указано';
         document.getElementById('lastName').textContent = user.last_name || 'Не указано';
         document.getElementById('username').textContent = user.username || 'Не указано';
